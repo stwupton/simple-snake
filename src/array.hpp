4e0b34b9d01c0096ff2d1574fdafed978b0745ee
Steven Upton <supton.sz@gmail.com>
2023-04-10 15:15:29 +0100

    fix indentation

--- src/array.hpp
+++ src/array.hpp
@@ -4,47 +4,47 @@
 
 template<typename T, size_t Size>
 struct Array {
-	Array() {}
+  Array() {}
 
-	size_t length = 0;
-	T data[Size];
+  size_t length = 0;
+  T data[Size];
 
-	T &operator [](size_t index) {
-		assert(index >= 0 && index < Size);
-		return this->data[index];
-	}
+  T &operator [](size_t index) {
+    assert(index >= 0 && index < Size);
+    return this->data[index];
+  }
 
-	T operator [](size_t index) const {
-		assert(index >= 0 && index < Size);
-		return this->data[index];
-	}
+  T operator [](size_t index) const {
+    assert(index >= 0 && index < Size);
+    return this->data[index];
+  }
 
-	T *begin() {
-		return this->data;
-	}
+  T *begin() {
+    return this->data;
+  }
 
-	const T *begin() const {
-		return this->data;
-	}
+  const T *begin() const {
+    return this->data;
+  }
 
-	void clear() {
-		this->length = 0;
-	}
+  void clear() {
+    this->length = 0;
+  }
 
-	T *end() {
-		return &this->data[this->length];
-	}
+  T *end() {
+    return &this->data[this->length];
+  }
 
-	const T *end() const {
-		return &this->data[this->length];
-	}
+  const T *end() const {
+    return &this->data[this->length];
+  }
 
-	T pop() {
-		return this->data[--this->length];
-	}
+  T pop() {
+    return this->data[--this->length];
+  }
 
-	T &push(T value) {
-		assert(this->length != Size);
-		return this->data[this->length++] = value;
-	}
+  T &push(T value) {
+    assert(this->length != Size);
+    return this->data[this->length++] = value;
+  }
 };
--- src/vector.hpp
+++ src/vector.hpp
@@ -6,115 +6,115 @@
 
 template<typename T>
 struct Vec2 { 
-	T x, y;
+  T x, y;
 
-	Vec2(T x = 0, T y = 0) : x(x), y(y) {}
+  Vec2(T x = 0, T y = 0) : x(x), y(y) {}
 
-	Vec2<T> operator -(const Vec2<T> &other) const {
-		return Vec2<T>(this->x - other.x, this->y - other.y);
-	}
+  Vec2<T> operator -(const Vec2<T> &other) const {
+    return Vec2<T>(this->x - other.x, this->y - other.y);
+  }
 
-	Vec2<T> operator +(const Vec2<T> &other) const {
-		return Vec2<T>(this->x + other.x, this->y + other.y);
-	}
+  Vec2<T> operator +(const Vec2<T> &other) const {
+    return Vec2<T>(this->x + other.x, this->y + other.y);
+  }
 
-	Vec2<T> operator +=(const Vec2<T> &other) {
-		return *this = *this + other;
-	}
+  Vec2<T> operator +=(const Vec2<T> &other) {
+    return *this = *this + other;
+  }
 
-	Vec2<T> operator *(T x) const {
-		return Vec2<T>(this->x * x, this->y * x);
-	}
+  Vec2<T> operator *(T x) const {
+    return Vec2<T>(this->x * x, this->y * x);
+  }
 
-	bool operator ==(const Vec2<T> &other) const {
-		return this->x == other.x && this->y == other.y;
-	}
+  bool operator ==(const Vec2<T> &other) const {
+    return this->x == other.x && this->y == other.y;
+  }
 
-	f32 distanceTo(const Vec2<T> &other) const {
-		return sqrt(pow(this->x - other.x, 2) + pow(this->y - other.y, 2));
-	}
+  f32 distanceTo(const Vec2<T> &other) const {
+    return sqrt(pow(this->x - other.x, 2) + pow(this->y - other.y, 2));
+  }
 
-	T magnitude() const {
-		return sqrt(pow(this->x, 2) + pow(this->y, 2));
-	}
+  T magnitude() const {
+    return sqrt(pow(this->x, 2) + pow(this->y, 2));
+  }
 
-	Vec2<T> normalized() const {
-		T magnitude = this->magnitude();
-		return Vec2<T>(this->x / magnitude, this->y / magnitude);
-	}
+  Vec2<T> normalized() const {
+    T magnitude = this->magnitude();
+    return Vec2<T>(this->x / magnitude, this->y / magnitude);
+  }
 };
 
 template<typename T>
 struct Vec3 : Vec2<T> { 
-	T z;
-	Vec3(T x = 0, T y = 0, T z = 0) : Vec2<T>(x, y), z(z) {}
+  T z;
+  Vec3(T x = 0, T y = 0, T z = 0) : Vec2<T>(x, y), z(z) {}
 
-	Vec3<T> operator -(const Vec3<T> &other) const {
-		return Vec3<T>(this->x - other.x, this->y - other.y, this->z - other.z);
-	}
+  Vec3<T> operator -(const Vec3<T> &other) const {
+    return Vec3<T>(this->x - other.x, this->y - other.y, this->z - other.z);
+  }
 
-	Vec3<T> operator +(const Vec3<T> &other) const {
-		return Vec3<T>(this->x + other.x, this->y + other.y, this->z + other.z);
-	}
+  Vec3<T> operator +(const Vec3<T> &other) const {
+    return Vec3<T>(this->x + other.x, this->y + other.y, this->z + other.z);
+  }
 
-	Vec3<T> &operator +=(const Vec3<T> &other) {
-		return *this = *this + other;
-	}
+  Vec3<T> &operator +=(const Vec3<T> &other) {
+    return *this = *this + other;
+  }
 
-	Vec3<T> operator *(T x) const {
-		return Vec3<T>(this->x * x, this->y * x, this->z * x);
-	}
+  Vec3<T> operator *(T x) const {
+    return Vec3<T>(this->x * x, this->y * x, this->z * x);
+  }
 
-	f32 distanceTo(const Vec3<T> &other) const {
-		return sqrt(
-			pow(this->x - other.x, 2) + 
-			pow(this->y - other.y, 2) + 
-			pow(this->z - other.z, 2) 
-		);
-	}
+  f32 distanceTo(const Vec3<T> &other) const {
+    return sqrt(
+      pow(this->x - other.x, 2) + 
+      pow(this->y - other.y, 2) + 
+      pow(this->z - other.z, 2) 
+    );
+  }
 
-	T magnitude() const {
-		return sqrt(pow(this->x, 2) + pow(this->y, 2) + pow(this->z, 2));
-	}
+  T magnitude() const {
+    return sqrt(pow(this->x, 2) + pow(this->y, 2) + pow(this->z, 2));
+  }
 
-	Vec3<T> normalized() const {
-		T magnitude = this->magnitude();
-		return Vec3<T>(this->x / magnitude, this->y / magnitude, this->z / magnitude);
-	}
+  Vec3<T> normalized() const {
+    T magnitude = this->magnitude();
+    return Vec3<T>(this->x / magnitude, this->y / magnitude, this->z / magnitude);
+  }
 };
 
 struct Rgb {
-	f32 r, g, b;
-	Rgb(f32 r = 0, f32 g = 0, f32 b = 0) : r(r), g(g), b(b) {}
+  f32 r, g, b;
+  Rgb(f32 r = 0, f32 g = 0, f32 b = 0) : r(r), g(g), b(b) {}
 };
 
 struct Rgba : Rgb {
-	f32 a;
-	Rgba(f32 r = 0, f32 g = 0, f32 b = 0, f32 a = 0) : Rgb(r, g, b), a(a) {}
+  f32 a;
+  Rgba(f32 r = 0, f32 g = 0, f32 b = 0, f32 a = 0) : Rgb(r, g, b), a(a) {}
 
-	Rgba operator +(const Rgba &other) const {
-		return Rgba(
-			this->r + other.r, 
-			this->g + other.g, 
-			this->b + other.b, 
-			this->a + other.a
-		);
-	}
+  Rgba operator +(const Rgba &other) const {
+    return Rgba(
+      this->r + other.r, 
+      this->g + other.g, 
+      this->b + other.b, 
+      this->a + other.a
+    );
+  }
 
-	Rgba operator -(const Rgba &other) const {
-		return Rgba(
-			this->r - other.r, 
-			this->g - other.g, 
-			this->b - other.b, 
-			this->a - other.a
-		);
-	}
+  Rgba operator -(const Rgba &other) const {
+    return Rgba(
+      this->r - other.r, 
+      this->g - other.g, 
+      this->b - other.b, 
+      this->a - other.a
+    );
+  }
 
-	Rgba &operator +=(const Rgba &other) {
-		return *this = *this + other;
-	}
+  Rgba &operator +=(const Rgba &other) {
+    return *this = *this + other;
+  }
 
-	Rgba &operator -=(const Rgba &other) {
-		return *this = *this - other;
-	}
+  Rgba &operator -=(const Rgba &other) {
+    return *this = *this - other;
+  }
 };
